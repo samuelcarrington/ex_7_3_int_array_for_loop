b9aa5de31fe636373c33dfcdbd832064331f787b
Samuel Carrington <samuelcarrington@gmail.com>
2016-01-15 18:12:54 -0500

    meaningful commit message

---
+++ ex_7_3_int_array_for_loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_7_3_int_array_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program, which creates an array of 20 elements 
+            of type integer and initializes each of the elements with 
+            a value equals to the index of the element multiplied by 5. 
+            Print the elements to the console. (Chapter 7, Exercise 1)
+            */
+
+            int[]
+
+                for (int i = 0; i < length; i++)
+            {
+
+            }
+
+    
+        }
+    }
+}
