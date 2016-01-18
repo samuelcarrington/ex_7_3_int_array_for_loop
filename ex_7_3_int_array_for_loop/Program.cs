5f8d1659ded7697f6af458145db96cf1cbaab7cf
Samuel Carrington <samuelcarrington@gmail.com>
2016-01-18 08:57:48 -0500

    revision

--- ex_7_3_int_array_for_loop/Program.cs
+++ ex_7_3_int_array_for_loop/Program.cs
@@ -16,14 +16,17 @@ namespace ex_7_3_int_array_for_loop
             Print the elements to the console. (Chapter 7, Exercise 1)
             */
 
-            int[]
-
-                for (int i = 0; i < length; i++)
+            //creates an array of 20 elements of type integer
+            int[] multiplied = new int[20];
+            //and initializes each of the elements
+            for (int i = 0; i < 20; i++)        
             {
-
+                //with a value equals to the index of the element multiplied by 5
+                multiplied[i] = i * 5;
+                //print the elements to the console           
+                Console.Write(multiplied[i] + " "); 
             }
-
-    
+            Console.ReadKey();
         }
     }
 }
